b621c95db32f6cd1079094cfe3f9f21992bee0bc
Adrian Scheit <adrianscheit@gmail.com>
2025-02-16 21:58:51 +0100

    Remove a the min shift SVM model as it clearly did not work

--- src/main.cpp
+++ src/main.cpp
@@ -21,17 +21,16 @@ void calcForAngle(const long double angle)
         center = (vMin + vMax) / 2,
         svmPwm0 = (v[0] - vMin) / maxAmplitude,
         svmPwm1 = (vMax - v[0]) / maxAmplitude,
-        svmPwmCenter = (v[0] - center) / maxAmplitude + 0.5,
-        svmPwmMinShift = vMin < -vMax ? svmPwm0 : svmPwm1;
+        svmPwmCenter = (v[0] - center) / maxAmplitude + 0.5;
 
-    csv << angle << ',' << v[0] << ',' << vMin << ',' << vMax << ',' << center << ',' << svmPwm0 << ',' << svmPwm1 << ',' << svmPwmCenter << ',' << svmPwmMinShift << ',' << svmPwm1 << endl;
+    csv << angle << ',' << v[0] << ',' << vMin << ',' << vMax << ',' << center << ',' << svmPwm0 << ',' << svmPwm1 << ',' << svmPwmCenter << ',' << svmPwm1 << endl;
     mapSvmPwm0 << (unsigned int)(uint8_t)round(svmPwm0 * 255) << "," << endl;
 }
 
 int main()
 {
     cout << "angle120=" << angle120 << ", maxAmplitude" << maxAmplitude << endl;
-    csv << "angle,sin,vMin,vMax,center,svmPwm0,svmPwm1,svmPwmCenter,svmPwmMinShift" << endl;
+    csv << "angle,sin,vMin,vMax,center,svmPwm0,svmPwm1,svmPwmCenter" << endl;
     mapSvmPwm0 << "const uint8_t map[] = {";
 
     for (int i = 0; i < 256; ++i)
